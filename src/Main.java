58c817699d910fc932b6708b8b28ac14663559e1
hectorvlbs28 <98681806+hectorvlbs28@users.noreply.github.com>
2024-11-30 22:47:06 -0700

    Lesson 5: Reading and Creating Data with JDBC

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,30 @@
 import javax.swing.plaf.nimbus.State;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
 
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost/JavaJDBCApi", "root", "0+%1,npHD7O@9vz^WV2p#3LCY10xA*S_");
             System.out.println("Connected to the database");
 
             statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM users");
+            resultSet = statement.executeQuery("SELECT * FROM customers");
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getInt("id") + " " + resultSet.getString("name") + " " + resultSet.getString("email"));
+                System.out.println(resultSet.getInt("customerNumber") + " " + resultSet.getString("customerName"));
             }
+
+            String sql = ("INSERT INTO employees(employeeNumber,lastName,firstName,extension,email,officeCode,reportsTo,jobTitle) VALUES (100220,'Martinez','Liam','x5800','dmurphy@classicmodelcars.com','1',NULL,'President')");
+            preparedStatement = connection.prepareStatement(sql);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            System.out.println("Rows affected: " + rowsAffected);
         } catch (Exception e) {
             System.out.println("An error occurred.");
             throw new RuntimeException(e);
