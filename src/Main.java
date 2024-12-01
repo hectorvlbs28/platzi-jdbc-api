69f7c40763cc286036e164ee1686a85abefa3a6c
hectorvlbs28 <98681806+hectorvlbs28@users.noreply.github.com>
2024-11-30 22:24:13 -0700

    Lesson 4: Connecting Java to Databases

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import javax.swing.plaf.nimbus.State;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/JavaJDBCApi", "root", "0+%1,npHD7O@9vz^WV2p#3LCY10xA*S_");
+            System.out.println("Connected to the database");
+
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM users");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id") + " " + resultSet.getString("name") + " " + resultSet.getString("email"));
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            throw new RuntimeException(e);
+        }
     }
 }
